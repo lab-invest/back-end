d1c0d065801bc03315badf49bd1e0b6da7df6504
Bruno <bruno.germinatech@gmail.com>
2024-10-02 10:40:18 -0300

    dockerfile

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.include_router(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
